735e0c183473881f08eedabebab3d3a235fd37bc
ondreali <66229664+ondreali@users.noreply.github.com>
2020-06-21 00:10:36 -0500

    pass

--- test_basic_list.py
+++ test_basic_list.py
@@ -3,7 +3,7 @@ program: basic_list
 Author: Ondrea Li
 Last date modfied: 06/20/20
 
-The purpose of this program is to write the function that asks for 3 user input in a loop.
+The purpose of this program is to test basic_list.py
 """
 import unittest
 from unittest.mock import patch
@@ -11,10 +11,11 @@ from fun_with_collections import basic_list as topic1
 
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.topic1.get_input', return_value='5')
+    @patch('fun_with_collections.basic_list.get_input', return_value='5')
     def test_make_list(self, input):
         self.assertEqual(topic1.make_list(), [5, 5, 5])
 
 
 
 if __name__ == '__main__':
+    unittest.main()
